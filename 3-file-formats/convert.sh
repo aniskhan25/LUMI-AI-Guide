c80b5467337cedec23233297e0c322cdeaa4fc35
Anis Ur Rahman <aniskhan25@gmail.com>
2026-01-30 16:00:52 +0200

    Set Slurm output to default name in convert.sh

--- 3-file-formats/convert.sh
+++ 3-file-formats/convert.sh
@@ -5,6 +5,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=1750
 #SBATCH --time=0:10:00
+#SBATCH --output=slurm-%j.out
 
 module use /appl/local/training/modules/AI-20240529
 module load singularity-userfilesystems singularity-CPEbits
