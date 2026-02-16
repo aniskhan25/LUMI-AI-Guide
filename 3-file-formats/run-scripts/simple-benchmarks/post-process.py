63107a0c66624f6a905399a24b59abe838600b3b
Anis Ur Rahman <aniskhan25@gmail.com>
2026-02-16 15:10:35 +0200

    Simplify benchmark post-processing inputs

--- 3-file-formats/run-scripts/simple-benchmarks/post-process.py
+++ 3-file-formats/run-scripts/simple-benchmarks/post-process.py
@@ -1,6 +1,8 @@
+import os
 import sys, argparse
-from glob import glob
 import numpy as np
+
+from glob import glob
 
 # Raw data files can be summarized with CLI 'grep time *tiny*.out'
 
@@ -14,16 +16,23 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-files = glob(f"run-scripts/simple-benchmarks/*{args.data}*.out")
+data_tag = args.data
+project = os.environ.get("PROJECT_ACCOUNT", "project_462000131")
+user = os.environ.get("LUMI_USER", os.environ.get("USER", "anisrahm"))
+
+files = glob(f"/scratch/{project}/{user}/slurm/comp-{data_tag}-*.out")
 raw_result = {"HDF5": [], "LMDB": [], "SquashFS": []}
 for file_name in files:
     with open(file_name, "r") as fd:
-        lines = [line.rstrip("\n") for line in fd]
-        try:
-            file_format, _, _, time = lines[4].split(" ")
-        except Exception as e:
+        lines = [line.rstrip("\n") for line in fd if "dataloader time:" in line]
+        if not lines:
             continue
-        raw_result[file_format].append(float(time))
+        line = lines[-1]
+        try:
+            file_format, time = line.split(" dataloader time: ")
+            raw_result[file_format].append(float(time))
+        except Exception:
+            continue
 
 result = {
     "HDF5": {"average": 0, "std": 0, "N": 0},
