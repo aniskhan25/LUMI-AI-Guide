619ac734060279c8df5243e6c3656b77471c75d6
Anis Ur Rahman <aniskhan25@gmail.com>
2026-02-16 14:01:59 +0200

    Fix LMDB dataset byte decoding with PIL

--- 3-file-formats/scripts/lmdb/lmdb_dataset.py
+++ 3-file-formats/scripts/lmdb/lmdb_dataset.py
@@ -4,6 +4,7 @@ from torchvision.transforms.functional import to_pil_image
 import lmdb
 import pickle
 import os, six
+import io
 from PIL import Image
 
 
@@ -38,7 +39,7 @@ class LMDBDataset(Dataset):
             byteflow = txn.get(self.keys[idx])
         byte_image, label = pickle.loads(byteflow)
 
-        image = Image.open(byte_image).convert("RGB")
+        image = Image.open(io.BytesIO(byte_image)).convert("RGB")
         label = torch.tensor(label, dtype=torch.long)
 
         if self.transform:
